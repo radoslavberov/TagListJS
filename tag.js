a212b848428190eb68ad3a6e63f2514a66a7c380
Radoslav Berov <radoslavv.berov@gmail.com>
2019-06-02 22:27:37 +0300

    Added enter button and backspace

--- tag.js
+++ tag.js
@@ -4,7 +4,15 @@ const input = document.querySelector('.tag-container input');
 
 var tags = [];
 
+const enterButton = document.querySelector('#enter')
 
+enterButton.addEventListener('click', function(e) {
+  tags.push(input.value);
+  addTags();
+  input.value = '';
+})
+
+//the createTag function. It creates tag dynamicly in the html
 function createTag(label) {
   const div = document.createElement('div');
   div.setAttribute('class', 'tag');
@@ -17,9 +25,11 @@ function createTag(label) {
 
   div.appendChild(span);
   div.appendChild(closeBtn);
+  //appends the span and closeBtn to the div as his children
   return div;
 }
 
+//removes the last child which was created
 function resetTags() {
   document.querySelectorAll('.tag').forEach(function(tag){
     tag.parentElement.removeChild(tag);
@@ -30,15 +40,10 @@ function addTags(){
   resetTags();
   tags.slice().reverse().forEach(function(tag) {
     const input = createTag(tag)
-      tagContainer.prepend(input);
-    });
+    tagContainer.prepend(input);
+  });
 }
-// document.querySelector('#button').addEventListener('keypress',function (e) {
-//  var key = e.which || e.keyCode
-//  if (key == 13) {
-//    addTags();
-//  }
-//})
+
 input.addEventListener('keyup',function (e) {
   if (e.key == 'Enter') {
     tags.push(input.value);
@@ -48,10 +53,18 @@ input.addEventListener('keyup',function (e) {
 } )
 
 document.addEventListener('click', function (e) {
-  if (e.target.tagName === 'I') {
-    const value = e.target.getAttribute('data-item');
-    const index = tags.indexOf(value);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    addTags();
+ if (e.target.tagName === 'I') {
+   const value = e.target.getAttribute('data-item');
+   const index = tags.indexOf(value);
+   tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+   addTags();
   }
 })
+
+document.addEventListener('keyup',function (e) {
+  if (e.key == 'Backspace') {
+
+    tags = tags.slice(0, tags.length - 1)
+    addTags()
+  }
+} )
